3120d1a7ddffaee73cba3aa88965e0ee418950f5
Gage Smith <gagesmith242@gmail.com>
2026-02-03 16:34:26 -0700

    Installed playwright, and wrote a test for register

---
+++ tests/auth.spec.ts
@@ -0,0 +1,14 @@
+import { test, expect } from 'playwright-test-coverage';
+
+test('register', async ({ page }) => {
+  await page.goto('http://localhost:5173/');
+
+  await page.getByRole('link', { name: 'Register' }).click();
+  await page.getByRole('textbox', { name: 'Full name' }).fill('george');
+  await page.getByRole('textbox', { name: 'Email address' }).click();
+  await page.getByRole('textbox', { name: 'Email address' }).fill('george@gmail.com');
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill('curious');
+  await page.getByRole('button', { name: 'Register' }).click();
+  await expect(page.getByRole('link', { name: 'g', exact: true })).toBeVisible();
+});
