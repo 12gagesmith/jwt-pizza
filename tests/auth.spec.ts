cc6e56efb0b59a7d06ec81436eae09d06cf63afc
Gage Smith <gagesmith242@gmail.com>
2026-02-08 18:35:59 -0700

    Added test for loggin in and out

--- tests/auth.spec.ts
+++ tests/auth.spec.ts
@@ -12,3 +12,16 @@ test('register', async ({ page }) => {
   await page.getByRole('button', { name: 'Register' }).click();
   await expect(page.getByRole('link', { name: 'g', exact: true })).toBeVisible();
 });
+
+test('loginLogout', async ({ page }) => {
+  await page.goto('http://localhost:5173/');
+
+  await page.getByRole('link', { name: 'Login' }).click();
+  await page.getByRole('textbox', { name: 'Email address' }).fill('george@gmail.com');
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill('curious');
+  await page.getByRole('button', { name: 'Login' }).click();
+  await expect(page.getByRole('link', { name: 'g', exact: true })).toBeVisible();
+  await page.getByRole('link', { name: 'Logout' }).click();
+  await expect(page.getByRole('link', { name: 'Login' })).toBeVisible();
+});
