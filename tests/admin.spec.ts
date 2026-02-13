07f240a62aa3e454c8276ccfa75202201166eae2
Gage Smith <gagesmith242@gmail.com>
2026-02-12 22:54:11 -0700

    Added close tests for franchise and store

--- tests/admin.spec.ts
+++ tests/admin.spec.ts
@@ -13,7 +13,7 @@ test('viewAdminPage', async ({page}) => {
     await expect(page.getByText('AdminPerson')).toBeVisible();
 });
 
-test('createFranchise', async ({page}) => {
+test('createAndCloseFranchise', async ({page}) => {
     await basicInit(page);
     await page.getByRole('link', { name: 'Login' }).click();
     await page.getByRole('textbox', { name: 'Email address' }).click();
@@ -24,4 +24,7 @@ test('createFranchise', async ({page}) => {
     await page.getByRole('link', { name: 'Admin' }).click();
     await page.getByRole('button', { name: 'Add Franchise' }).click();
     await expect(page.getByText('Want to create franchise?')).toBeVisible();
+    await page.getByRole('button', { name: 'Cancel' }).click();
+    await page.getByRole('row', { name: 'LotaPizza Close' }).getByRole('button').click();
+    await expect(page.getByText('Sorry to see you go')).toBeVisible();
 });
--- tests/franchise.spec.ts
+++ tests/franchise.spec.ts
@@ -16,7 +16,7 @@ test('viewFranchisePage', async ({page}) => {
     await expect(page.getByText('Create store')).toBeVisible();
 });
 
-test('CreateStore', async ({page}) => {
+test('createStore', async ({page}) => {
     await basicInit(page);
     await page.getByRole('link', { name: 'Login' }).click();
     await page.getByRole('textbox', { name: 'Email address' }).fill('f@jwt.com');
@@ -29,4 +29,18 @@ test('CreateStore', async ({page}) => {
     await expect(page.getByText('Everything you need to run an')).toBeVisible();
     await page.getByRole('button', { name: 'Create store' }).click();
     await expect(page.getByText('homefranchise-dashboardcreate')).toBeVisible();
+});
+
+test('closeStore', async ({page}) => {
+    await basicInit(page);
+    await page.getByRole('link', { name: 'Login' }).click();
+    await page.getByRole('textbox', { name: 'Email address' }).fill('f@jwt.com');
+    await page.getByRole('textbox', { name: 'Password' }).click();
+    await page.getByRole('textbox', { name: 'Password' }).fill('franchisee');
+    await page.getByRole('button', { name: 'Login' }).click();
+    await page.waitForLoadState('networkidle');
+    await page.getByLabel('Global').getByRole('link', { name: 'Franchise' }).click();
+    await page.waitForLoadState('networkidle');
+    await page.getByRole('row', { name: 'BYU 0.033 ₿ Close' }).getByRole('button').click();
+    await expect(page.getByText('Sorry to see you go')).toBeVisible();
 });
