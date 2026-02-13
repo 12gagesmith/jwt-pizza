45e2c757997c1b87e6bd4e8caf8a80a0131a5a51
Gage Smith <gagesmith242@gmail.com>
2026-02-12 22:45:20 -0700

    Modified order.spec.ts to use mocks

--- tests/order.spec.ts
+++ tests/order.spec.ts
@@ -1,25 +1,23 @@
 import { test, expect } from 'playwright-test-coverage';
+import { basicInit } from './mocks';
 
-test('orderPizzaAndVerify', async ({ page }) => {
+test('orderPizza', async ({ page }) => {
     test.setTimeout(10000);
-    await page.goto('http://localhost:5173/');
+    await basicInit(page);
   
     await page.getByRole('link', { name: 'Login' }).click();
-    await page.getByRole('textbox', { name: 'Email address' }).fill('george@gmail.com');
+    await page.getByRole('textbox', { name: 'Email address' }).fill('d@jwt.com');
     await page.getByRole('textbox', { name: 'Password' }).click();
-    await page.getByRole('textbox', { name: 'Password' }).fill('curious');
+    await page.getByRole('textbox', { name: 'Password' }).fill('a');
     await page.getByRole('button', { name: 'Login' }).click();
     
     await page.waitForLoadState('networkidle');
     await page.getByRole('link', { name: 'Order' }).click();
     await page.waitForLoadState('networkidle');
     await page.getByRole('combobox').waitFor({ state: 'visible' });
-    await page.getByRole('combobox').selectOption('2');
+    await page.getByRole('combobox').selectOption('4');
     await page.getByRole('link', { name: 'Image Description Pepperoni' }).click();
     await page.getByRole('button', { name: 'Checkout' }).click();
     await page.getByRole('button', { name: 'Pay now' }).click();
-    await page.getByRole('button', { name: 'Verify' }).waitFor({ state: 'visible' });
-    await page.getByRole('button', { name: 'Verify' }).click();
-    await page.waitForTimeout(500);
-    await expect(page.getByRole('heading', { name: 'JWT Pizza - valid' })).toBeVisible();
+    await expect(page.getByText('Here is your JWT Pizza!')).toBeVisible();
   });
